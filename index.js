8782dd8a39a7dd2866e42b7ff1ac69c4e6187ef6
edus124 <edus124@gmail.com>
2022-09-24 17:53:25 -0500

    Ejecución de clasificación
    Se agrega una ejecución básica de la clasificación, usando un descriptor de pixeles planos.

--- index.js
+++ index.js
@@ -6,15 +6,6 @@ const fs = require("fs")
 const path = require("path")
 const cv = require('opencv4nodejs')  
 
-//Para instalar opencv4nodejs:
-//https://www.npmjs.com/package/opencv4nodejs
-//npm install --global windows-build-tools
-//npm install --save opencv4nodejs
-// si no funciona npm install --global windows-build-tools:
-// Desinstalar otras versiones de python, o instalar 2.7.17 y modificar el path, visual studio 2017 para compilar
-
-const Jimp = require('jimp');
-
 const getAllFiles = function(dirPath, arrayOfFiles){
     files = fs.readdirSync(dirPath)
     arrayOfFiles = arrayOfFiles || []
@@ -28,3 +19,47 @@ const getAllFiles = function(dirPath, arrayOfFiles){
     })
     return arrayOfFiles
 }
+
+const createImageFeatures = function(image) {
+  // # redimensionar imagenes
+  mat = image.resize(32, 32);
+  // # convertir en un solo array
+  let pixel_list = mat.getdataasarray().flat().flat();
+}
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {root: __dirname}); 
+  console.log("Reading all images");
+  let image_paths = getAllFiles("D://Bibliotecas//Descargas//train32")
+  let raw_images = []
+  let labels = []
+
+  image_paths.forEach(function(image_path) {
+    
+    let image = cv.imread(image_path);
+    let label = image_path.split('\\')[4];
+    let pixels = createImageFeatures(image)
+    raw_images.push(pixels);
+    labels.push(label);
+  });
+
+  let image = cv.imread("D://Bibliotecas//Descargas//cosko.jpeg")
+  let busq = createImageFeatures(image)  
+
+  console.log("Datos Cargados");
+  var root = kdtree.build_kdtree( raw_images, labels ) ;
+
+  // console.log("Ejecutando closest_point");
+  // best1 = kdtree.closest_point(root, busq); 
+  // console.log('BEST:');
+  // console.log(best1);
+
+  console.log('MEJORES K COINCIDENCIAS:')
+  bests = kdtree.KNN(raw_images, labels, 5, busq);  
+  console.log(bests);
+  
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
